e4379e12443855361c36c59baecb276f15ec81a8
SWQJueLian <1105033640@qq.com>
2015-09-04 12:13:09 +0800

    程序信息查看批处理
    查看包名等

---
+++ 程序信息查看直接将apk拖入这个文件内.bat
@@ -0,0 +1,25 @@
+@echo off
+title PackName 和 ClassName 查看批处理--By JueLian
+color 3f
+:menu
+echo.
+echo -------------------------------------------------------------------
+echo -- PackName 和 ClassName内容:                                    --
+echo -------------------------------------------------------------------
+echo.
+rem 这里是查看命令
+aapt dump badging %1
+echo.
+echo -------------------------------------------------------------------
+echo.
+echo 1：继续查看！
+echo.
+echo 2：退出程序！
+echo.
+echo -------------------------------------------------------------------
+set /p choice=请选择后面的操作！
+if /i "%choice%"=="1" goto menu
+if /i "%choice%"=="1" goto exit
+
+：exit
+exit
